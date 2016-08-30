87b268e974a0420a6a7f5555e1c6edd6d6972e5d
neocarto <nicolas.lambert@ums-riate.fr>
2016-08-30 11:47:12 +0200

    map update

--- brexit/brexit.R
+++ brexit/brexit.R
@@ -2,7 +2,7 @@ library("rgdal")
 library("rgeos")
 library("cartography")
 
-setwd("/home/nlambert/Documents/ReproducibleCartography/brexit")
+setwd("/home/nlambert/Documents/R/ReproducibleCartography/brexit")
 
 # import
 eu <- readOGR(dsn ="input",layer = "eu")
@@ -34,23 +34,34 @@ barycentre <- function(df, spdf,year, area){
 # data building
 
 centres.df <- data.frame(id=character(),x=integer(),y=integer(),nb=integer())
+
 area1957 <- c("R1","FR","BE","IT","LU","NL")
 centres.df <- barycentre(centres.df, eu,1957,area1957)
+
 area1973 <- c(area1957,"UK","IE","DK")
 centres.df <- barycentre(centres.df, eu,1973,area1973)
+
 area1981 <- c(area1973,"EL")
 centres.df <- barycentre(centres.df, eu,1981,area1981)
+
 area1986 <- c(area1981,"ES","PT")
-area1990 <- c(area1986[-which(area1986=="R1")],"DE") 
 centres.df <- barycentre(centres.df, eu,1986,area1986)
-area1995 <- c(area1990,"AT","SE","FI")
+
+area1990 <- c(area1986[-which(area1986=="R1")],"DE") 
 centres.df <- barycentre(centres.df, eu,1990,area1990)
+
+area1995 <- c(area1990,"AT","SE","FI")
+centres.df <- barycentre(centres.df, eu,1995,area1995)
+
 area2004 <- c(area1995,"CY", "MT", "EE", "HU", "LT", "LV", "PL", "CZ", "SK", "SI")
 centres.df <- barycentre(centres.df, eu,2004,area2004)
+
 area2007 <-c(area2004,"BG","RO")
 centres.df <- barycentre(centres.df, eu,2007,area2007)
+
 area2013 <-c(area2007,"HR")
 centres.df <- barycentre(centres.df, eu,2013,area2013)
+
 area2016 <-area2013[-which(area2013=="UK")]
 centres.df <- barycentre(centres.df, eu,2016,area2016)
 
